3f3c051665bd5704f740bd78a4e3d795f3727a3d
rctgamer3 <rctgamer3@gmail.com>
2023-10-28 22:10:17 +0200

    Added leave script

---
+++ modules/helper/force_leave.js
@@ -0,0 +1,16 @@
+// START LEAVE
+
+// const guild_list = client.guilds.cache.map(guild => guild);
+// const whitelisted_servers = [314512031313035264, 793074374663995412];
+// guild_list.forEach((guild) => {
+// 	console.info(`This is Server ${guild.id}, name "${guild.name}".`)
+// 	if (!whitelisted_servers.includes(guild.id)) {
+// 		console.log(`...which is not on the whitelist! Leaving..`)
+// 		// guild.leave()
+// 	}
+// 	else {
+// 		console.log("...which is on the whitelist.");
+// 	}
+// });
+
+// END LEAVE
